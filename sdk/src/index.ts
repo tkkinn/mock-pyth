2b1488d864101ca105592deef4ebc724e2a6caf2
tkkinn <tkkinn@protonmail.com>
2024-10-15 02:19:27 +0800

    feat: fix bug, remove js file

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,1 +1,6 @@
 export * from './oracleClient';
+export * from './oracleClientConfig';
+export * from './pullOracleClient';
+export * from './pushOracleClient';
+export * from './pullOracleClientConfig';
+export * from './pushOracleClientConfig';
